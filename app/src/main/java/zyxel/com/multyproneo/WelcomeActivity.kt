f48c61e661bc8acfdf2c161fb9e28be79696974d
LouisTien <louis.tien@zyxel.com.tw>
2019-05-23 11:15:39 +0800

    complete welcome page

--- app/src/main/java/zyxel/com/multyproneo/WelcomeActivity.kt
+++ app/src/main/java/zyxel/com/multyproneo/WelcomeActivity.kt
@@ -2,10 +2,14 @@ package zyxel.com.multyproneo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.view.ViewPager
 import android.view.View
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_welcome.*
+import org.jetbrains.anko.startActivity
 import zyxel.com.multyproneo.adapter.WelcomeAdapter
+import zyxel.com.multyproneo.util.SharedPreferencesUtil
 
 /**
  * Created by LouisTien on 2019/5/21.
@@ -13,12 +17,16 @@ import zyxel.com.multyproneo.adapter.WelcomeAdapter
 class WelcomeActivity : AppCompatActivity()
 {
 
+    private val WELCOME_DISPLAY_TIME_IN_MILLISECONDS: Long = 1500
+    private val welcomeAdapter = WelcomeAdapter(this)
+    private val handler = Handler()
+    private var firstTimeUse by SharedPreferencesUtil(this, "key_first_time_use", true)
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        val welcomeAdapter = WelcomeAdapter(this)
         welcome_view_pager.adapter = welcomeAdapter
         welcome_view_pager.offscreenPageLimit = 3
         welcome_view_pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener
@@ -62,6 +70,59 @@ class WelcomeActivity : AppCompatActivity()
                 }
             }
         })
-        welcome_view_pager_indicator.attachTo(welcome_view_pager)
+
+        welcome_arrow_image.setOnClickListener{
+            when (welcome_view_pager.currentItem)
+            {
+                0 -> welcome_view_pager.currentItem = 1
+                1 -> welcome_view_pager.currentItem = 2
+                else ->{}
+            }}
+        welcome_right_start_text.setOnClickListener{launch()}
+        welcome_left_start_text.setOnClickListener{launch()}
+
+        handler.postDelayed({ startMainActivity()}, WELCOME_DISPLAY_TIME_IN_MILLISECONDS)
+
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+    }
+
+    private fun startMainActivity()
+    {
+        if (firstTimeUse)
+        {
+            launcher_linear.visibility = View.INVISIBLE
+            welcome_view_pager_linear.visibility = View.VISIBLE
+            welcome_view_pager.adapter = welcomeAdapter
+            welcome_view_pager_indicator.attachTo(welcome_view_pager)
+            welcome_left_item_linear.visibility = View.INVISIBLE
+            welcome_right_item_relative.visibility = View.VISIBLE
+            welcome_arrow_image.visibility = View.VISIBLE
+            welcome_right_start_text.visibility = View.INVISIBLE
+        }
+        else
+            launch()
+    }
+
+    private fun launch()
+    {
+        firstTimeUse = false
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        startActivity<MainActivity>()
+        finish()
     }
 }
